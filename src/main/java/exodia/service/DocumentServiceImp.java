0c60a4654962898e293e8f0a032cfa7d0e5d82d6
MargaritaSv <margarita.esti@gmail.com>
2019-03-09 15:00:04 +0200

    Exodia project

---
+++ src/main/java/exodia/service/DocumentServiceImp.java
@@ -0,0 +1,69 @@
+package exodia.service;
+
+import exodia.domain.entities.Document;
+import exodia.domain.models.service.DocumentServiceModel;
+import exodia.repository.DocumentRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DocumentServiceImp implements DocumentService {
+    private final DocumentRepository documentRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public DocumentServiceImp(DocumentRepository documentRepository, ModelMapper modelMapper) {
+        this.documentRepository = documentRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public DocumentServiceModel scheduleDocument(DocumentServiceModel documentServiceModel) {
+
+        Document document = this.modelMapper.map(documentServiceModel, Document.class);
+
+        try {
+            document = this.documentRepository.save(document);
+
+            return this.modelMapper.map(document, DocumentServiceModel.class);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public DocumentServiceModel findDocumentById(String id) {
+        Document doc = this.documentRepository.findById(id).orElse(null);
+
+        if (doc == null) {
+            return null;
+        }
+        return this.modelMapper.map(doc, DocumentServiceModel.class);
+    }
+
+    @Override
+    public List<DocumentServiceModel> findAllDocuments() {
+        return this.documentRepository.findAll()
+                .stream()
+                .map(d -> this.modelMapper.map(d, DocumentServiceModel.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean printDocumentsById(String id) {
+
+        try {
+            this.documentRepository.deleteById(id);
+            return  true;
+        } catch (Exception ex) {
+        ex.printStackTrace();
+        return false;
+        }
+    }
+}
